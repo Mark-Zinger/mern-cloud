b76f2fc8194a549d9272a5fb4c8d6a9b9869916e
Michail <lllessive@gmail.com>
2021-12-09 10:32:16 +0700

    express server, mongodb setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+
+const [PORT, DBNAME, DBUSER, DBPASS] = ["serverPort","db.name","db.user", "db.password"].map(key => config.get(key));
+const dbURL = `mongodb+srv://${DBUSER}:${DBPASS}@cloud.qzavu.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
+
+const start = async () => {
+    try {
+
+        await mongoose.connect(dbURL);
+
+        app.listen(PORT, () => {
+            console.log(`Server started on port: ${PORT}`);
+        })
+    } catch (e) {
+
+    }
+}
+
+start();
