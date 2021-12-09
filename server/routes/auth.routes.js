4e45e8c78782cd02e9bbeeb0e910f670721ebc80
Michail <lllessive@gmail.com>
2021-12-09 14:16:11 +0700

    add auth-logic jwt-token

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
+const congif = require("config");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -23,11 +25,36 @@ router.post('/registration',
 
             if(candidate) return res.status(400).json({message: `User with email ${email} already exist`});
 
-            const hashPassword = await bcrypt.hash(password, 15); 
+            const hashPassword = await bcrypt.hash(password, 8); 
             const user = new User({ email, password: hashPassword });
             await user.save();
             
             return res.json({message: 'User was created'})
+
+        } catch (e) {
+            console.log(e);
+            res.send({message: "Server Error"})
+        }
+    }
+);
+
+router.post('/login',
+    async (req,res) => {
+        try {
+
+            const { email, password } = req.body;
+
+            const user = ( await User.findOne({email}) ).toObject();
+            if(!user) return res.status(404).json({message: "User not found"});
+
+            const isPassValid = bcrypt.compareSync(password, user.password);
+            if(!isPassValid) return res.status(400).json({message: "Invalid password"});
+
+            const token = jwt.sign({ id: user.id }, congif.get("secretKey"), { expiresIn: "1h"});
+
+            delete user.password;
+
+            return res.json({ token, user });
 
         } catch (e) {
             console.log(e);
