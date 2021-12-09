ca2bdb13892f19700db825ad95d921dccd3073bc
Michail <lllessive@gmail.com>
2021-12-09 12:34:46 +0700

    create REST-API, registration, validation

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 
+const authRouter = require("./routes/auth.routes");
+
 const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 const [PORT, DBNAME, DBUSER, DBPASS] = ["serverPort","db.name","db.user", "db.password"].map(key => config.get(key));
 const dbURL = `mongodb+srv://${DBUSER}:${DBPASS}@cloud.qzavu.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
